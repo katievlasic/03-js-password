9ae82fc16e6ba2bda7c6b56ca32e427e613657dc
katievlasic <118415802+katievlasic@users.noreply.github.com>
2022-12-18 17:16:19 -0600

    create and begin defining function generatePassword

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # 03-js-password
+
+## Password Generator
+
+With a provided code, this applications purpose will be to generate a password where the user is provided with several options for what level of security the password will contain.
+
+The prompted choices will be:
+- Special Characters
+- Length (8-128 characters)
+- Upper vs. Lowercase present
+
+Following the prompts the password will either display in an alert or on the web application page.
+
--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,53 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
+const decide = {
+  ichar: function(x) {
+    window.prompt("Please enter how many characters the password needs to be:")
+  },
+  schar: function() {
+    window.prompt("Would you like special characters in your password?")
+  }, // how to force the inputs to be boolean???
+  lchar: function() {
+    window.prompt("Would you like lowercase characters in your password?")
+  },
+  uchar: function() {
+    window.prompt("Would you like uppercase characters in your password?")
+  }
+};
+
+
+function generatePassword() {
+  let userIchar = decide.ichar();
+  if (userIchar >= 128) {
+    alert("Please enter a number less than 128!");
+  }
+  else if (userIchar <= 128) {
+    userIchar.String.length;
+    console.log(Math.floor(Math.random()*x));
+  }
+  let userSchar = decide.schar();
+  if(userSchar) {
+    //no need to declare anything after userSchar since input is boolean
+    userSchar.toUppercase();
+  } else {
+    alert("Okay, no uppercase letters will be included.");
+  }
+  let userLchar = decide.lchar();
+  if (userLchar) {
+    userLchar.toLowercase();
+  }
+  else {
+    alert("Okay, no lowercase letters will be included.");
+  }
+  let userUchar = decide.uchar();
+  if (userUchar) {
+    userUchar.Math.value; //placeholder for now
+  }
+
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,5 +57,9 @@ function writePassword() {
 
 }
 
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+writePassword();
